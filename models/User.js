a4e23c248622f9571c159193ea49f0acd122bdf1
sean <seansaoirseh@gmail.com>
2020-02-27 20:31:30 +0100

    Register user API done

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  nama_depan: {
+    type: String,
+    required: true
+  },
+  nama_belakang: {
+    type: String,
+    required: true
+  },
+  paspor: {
+    type: String,
+    required: true
+  },
+  ponsel: {
+    type: String,
+    required: true
+  },
+  alamat: {
+    type: String,
+    required: true
+  },
+  kota_kodepos: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("user", UserSchema);
