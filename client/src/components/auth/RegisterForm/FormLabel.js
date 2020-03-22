dff5e7622b90f2dcd4af6e77810bc0b9de0a3ed3
sean <seansaoirseh@gmail.com>
2020-03-22 22:55:11 +0100

    FormLabel rendered properly with htmlFor attribute and proptypes defined as well as its default props.  (unit test)

--- client/src/components/auth/RegisterForm/FormLabel.js
+++ client/src/components/auth/RegisterForm/FormLabel.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import PropTypes from "prop-types";
-const FormLabel = ({ htmlFor }) => {
+const FormLabel = ({ htmlFor, text }) => {
   return <Form.Label htmlFor={htmlFor}></Form.Label>;
 };
 
 FormLabel.propTypes = {
   htmlFor: PropTypes.string.isRequired
 };
+
+FormLabel.defaultProps = {
+  htmlFor: "some html element"
+};
+
 export default FormLabel;
