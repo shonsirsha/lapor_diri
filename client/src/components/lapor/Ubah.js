4312b0dae1b422de3945f1d830c9dc48c880f476
sean <seansaoirseh@gmail.com>
2020-04-11 10:30:57 +0200

    added status banner for /ubah

--- client/src/components/lapor/Ubah.js
+++ client/src/components/lapor/Ubah.js
@@ -29,6 +29,8 @@ const Ubah = (props) => {
   } = authContext;
   const { setAlert, clearAllAlerts } = alertContext;
 
+  const [saved, setSaved] = useState(true);
+
   const [user, setUser] = useState({
     nama_depan: "",
     nama_belakang: "",
@@ -53,6 +55,7 @@ const Ubah = (props) => {
 
   const onChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
+    setSaved(false);
   };
 
   const onSubmit = (e) => {
@@ -92,141 +95,181 @@ const Ubah = (props) => {
     }
     //todo is to setUser for each var into its inputs.
   }, [authContext.user]);
+  if (isAuthenticated && !loading) {
+    return (
+      <Container style={{ marginTop: "32px", position: "relative" }}>
+        <Alert className='statusBanner' variant='success'>
+          Data tersimpan
+        </Alert>
+        <Row>
+          <Col>
+            <h2>Data {nama_depan + " " + nama_belakang} saat ini</h2>
+            <Button
+              style={{ marginTop: "8px" }}
+              variant='outline-success'
+              href='#unggah'
+            >
+              Unggah Dokumen{" "}
+            </Button>
+            <hr />
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{ marginBottom: "64px" }}>
+            <Accordion style={{ marginBottom: "16px" }} defaultActiveKey='0'>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey='0'>
+                  Data Diri
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey='0'>
+                  <Card.Body>
+                    <Form style={{ marginBottom: "32px" }} onSubmit={onSubmit}>
+                      <Form.Group>
+                        <FormLabel htmlFor='nama_depan' text='Nama Depan' />
+                        <FormInput
+                          inputName='nama_depan'
+                          inputType='text'
+                          onChangeMethod={onChange}
+                          value={nama_depan}
+                        />
+                      </Form.Group>
 
-  return (
-    <Container style={{ marginTop: "32px" }}>
-      <Row>
-        <Col>
-          <h2>Data Sean Liesanggoro saat ini</h2>
-          <Button style={{ marginTop: "8px" }} variant='success' href='#unggah'>
-            Unggah Dokumen{" "}
-            {
-              authContext.user ? authContext.user.first_name : "asdas" //should use the state here not the actual context.user
-            }
-          </Button>
-          <hr />
-        </Col>
-      </Row>
-      <Row>
-        <Col style={{ marginBottom: "64px" }}>
-          <Accordion style={{ marginBottom: "16px" }} defaultActiveKey='0'>
-            <Card>
-              <Accordion.Toggle as={Card.Header} eventKey='0'>
-                Data Diri
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey='0'>
-                <Card.Body>
-                  <Form style={{ marginBottom: "32px" }} onSubmit={onSubmit}>
-                    <Form.Group>
-                      <FormLabel htmlFor='nama_depan' text='Nama Depan' />
-                      <FormInput
-                        inputName='nama_depan'
-                        inputType='text'
-                        onChangeMethod={onChange}
-                        value={nama_depan}
-                      />
-                    </Form.Group>
+                      <Form.Group>
+                        <FormLabel
+                          htmlFor='nama_belakang'
+                          text='Nama Belakang'
+                        />
+                        <FormInput
+                          inputName='nama_belakang'
+                          inputType='text'
+                          onChangeMethod={onChange}
+                          value={nama_belakang}
+                        />
+                      </Form.Group>
 
-                    <Form.Group>
-                      <FormLabel htmlFor='nama_belakang' text='Nama Belakang' />
-                      <FormInput
-                        inputName='nama_belakang'
-                        inputType='text'
-                        onChangeMethod={onChange}
-                        value={nama_belakang}
-                      />
-                    </Form.Group>
+                      <Form.Group>
+                        <FormLabel htmlFor='paspor' text='Nomor Paspor' />
+                        <FormInput
+                          inputName='paspor'
+                          inputType='text'
+                          onChangeMethod={onChange}
+                          value={paspor}
+                        />
+                      </Form.Group>
 
-                    <Form.Group>
-                      <FormLabel htmlFor='paspor' text='Nomor Paspor' />
-                      <FormInput
-                        inputName='paspor'
-                        inputType='text'
-                        onChangeMethod={onChange}
-                        value={paspor}
-                      />
-                    </Form.Group>
-                  </Form>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
+                      <Button
+                        type='submit'
+                        style={{ marginTop: "8px" }}
+                        variant='success'
+                        href='#unggah'
+                      >
+                        Simpan & perbarui
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
 
-          <Accordion style={{ marginBottom: "16px" }} defaultActiveKey='0'>
-            <Card>
-              <Accordion.Toggle as={Card.Header} eventKey='0'>
-                Alamat di Jerman
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey='0'>
-                <Card.Body>
-                  <Form style={{ marginBottom: "32px" }} onSubmit={onSubmit}>
-                    <Form.Group>
-                      <FormLabel htmlFor='alamat' text='Alamat di Jerman' />
-                      <FormInput
-                        inputName='alamat'
-                        inputType='text'
-                        onChangeMethod={onChange}
-                        value={alamat}
-                      />
-                    </Form.Group>
+            <Accordion style={{ marginBottom: "16px" }} defaultActiveKey='0'>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey='0'>
+                  Alamat di Jerman
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey='0'>
+                  <Card.Body>
+                    <Form style={{ marginBottom: "32px" }} onSubmit={onSubmit}>
+                      <Form.Group>
+                        <FormLabel htmlFor='alamat' text='Alamat di Jerman' />
+                        <FormInput
+                          inputName='alamat'
+                          inputType='text'
+                          onChangeMethod={onChange}
+                          value={alamat}
+                        />
+                      </Form.Group>
 
-                    <Form.Group>
-                      <FormLabel htmlFor='kota_kodepos' text='Kodepos & Kota' />
-                      <FormInput
-                        inputName='kota_kodepos'
-                        inputType='text'
-                        onChangeMethod={onChange}
-                        value={kota_kodepos}
-                      />
-                    </Form.Group>
-                  </Form>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
+                      <Form.Group>
+                        <FormLabel
+                          htmlFor='kota_kodepos'
+                          text='Kodepos & Kota'
+                        />
+                        <FormInput
+                          inputName='kota_kodepos'
+                          inputType='text'
+                          onChangeMethod={onChange}
+                          value={kota_kodepos}
+                        />
+                      </Form.Group>
 
-          <Accordion defaultActiveKey='0'>
-            <Card>
-              <Accordion.Toggle as={Card.Header} eventKey='0'>
-                Kontak
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey='0'>
-                <Card.Body>
-                  <Form style={{ marginBottom: "32px" }} onSubmit={onSubmit}>
-                    <Form.Group>
-                      <FormLabel htmlFor='email' text='Email' />
-                      <FormInput
-                        inputName='email'
-                        inputType='text'
-                        onChangeMethod={onChange}
-                        value={email}
-                      />
-                    </Form.Group>
+                      <Button
+                        type='submit'
+                        style={{ marginTop: "8px" }}
+                        variant='success'
+                        href='#unggah'
+                      >
+                        Simpan & perbarui
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
 
-                    <Form.Group>
-                      <FormLabel htmlFor='ponsel' text='Ponsel' />
-                      <FormInput
-                        inputName='ponsel'
-                        inputType='text'
-                        onChangeMethod={onChange}
-                        value={ponsel}
-                      />
-                    </Form.Group>
-                  </Form>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
-        </Col>
-      </Row>
-      <Row id='unggah'>
-        <Col>
-          <hr />
-          <h2>Unggah dokumen</h2>
-        </Col>
-      </Row>
-    </Container>
-  );
+            <Accordion defaultActiveKey='0'>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey='0'>
+                  Kontak
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey='0'>
+                  <Card.Body>
+                    <Form style={{ marginBottom: "32px" }} onSubmit={onSubmit}>
+                      <Form.Group>
+                        <FormLabel htmlFor='email' text='Email' />
+                        <FormInput
+                          inputName='email'
+                          inputType='text'
+                          onChangeMethod={onChange}
+                          value={email}
+                        />
+                      </Form.Group>
+
+                      <Form.Group>
+                        <FormLabel htmlFor='ponsel' text='Ponsel' />
+                        <FormInput
+                          inputName='ponsel'
+                          inputType='text'
+                          onChangeMethod={onChange}
+                          value={ponsel}
+                        />
+                      </Form.Group>
+
+                      <Button
+                        type='submit'
+                        style={{ marginTop: "8px" }}
+                        variant='success'
+                        href='#unggah'
+                      >
+                        Simpan & perbarui
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+        </Row>
+        <Row id='unggah'>
+          <Col>
+            <hr />
+            <h2>Unggah dokumen</h2>
+          </Col>
+        </Row>
+      </Container>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 export default Ubah;
