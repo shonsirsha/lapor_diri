5070f9fa9e69ada48d7c51ea28f432233c135454
sean <seansaoirseh@gmail.com>
2020-04-23 21:34:34 +0200

    cekContext & cekReducer created

---
+++ client/src/context/cek/cekContext.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+const cekContext = createContext();
+
+export default cekContext;
---
+++ client/src/context/cek/cekReducer.js
@@ -0,0 +1,9 @@
+import { CHECK_REGISTRATION } from "../types";
+export default (state, action) => {
+  switch (action.type) {
+    case CHECK_REGISTRATION:
+      return state;
+    default:
+      return state;
+  }
+};
