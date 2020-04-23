e4592ebe4e814b47efeff77f2b2cb001a2c94982
sean <seansaoirseh@gmail.com>
2020-04-23 19:17:57 +0200

    checkUserExists.js moved into utils folder

--- routes/cek.js
+++ routes/cek.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
-const checkUserExists = require("./checkUserExists");
+const checkUserExists = require("./utils/checkUserExists");
 
 router.get(
   "/",
