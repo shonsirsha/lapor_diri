9c52c68b91e715a4afa606df999fe9bf178db8d8
sean <seansaoirseh@gmail.com>
2020-04-24 10:45:19 +0200

    added methods for toastState

--- client/src/context/toast/toastState.js
+++ client/src/context/toast/toastState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import { UPDATE_SUCCESS, UPDATE_FAIL } from "../types";
+import { UPDATE_SUCCESS, UPDATE_FAIL, RESET_UPDATE } from "../types";
 
 import ToastContext from "./toastContext";
 import ToastReducer from "./toastReducer";
@@ -7,6 +7,18 @@ import ToastReducer from "./toastReducer";
 const toastState = (props) => {
   const initialState = {
     updated: "hidden",
+  };
+
+  const updateSuccess = () => {
+    dispatch({ type: UPDATE_SUCCESS });
+  };
+
+  const updateFail = () => {
+    dispatch({ type: UPDATE_FAIL });
+  };
+
+  const resetToast = () => {
+    dispatch({ type: RESET_UPDATE });
   };
 
   const [state, dispatch] = useReducer(AuthReducer, initialState);
