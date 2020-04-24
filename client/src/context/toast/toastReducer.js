043e75108ac35c33df510d410241ae925e925dda
sean <seansaoirseh@gmail.com>
2020-04-24 10:46:54 +0200

    added cases for toastReducer

--- client/src/context/toast/toastReducer.js
+++ client/src/context/toast/toastReducer.js
@@ -1,10 +1,12 @@
-import { UPDATE_SUCCESS, UPDATE_FAIL } from "../types";
+import { UPDATE_SUCCESS, UPDATE_FAIL, RESET_UPDATE } from "../types";
 export default (state, action) => {
   switch (action.type) {
     case UPDATE_SUCCESS:
-      return state;
+      return { updated: "success" };
     case UPDATE_FAIL:
-      return state;
+      return { updated: "fail" };
+    case RESET_UPDATE:
+      return { updated: "hidden" };
     default:
       return state;
   }
