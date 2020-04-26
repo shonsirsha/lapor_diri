e7437dd61df20e4483a7aa27ec25f42bcf569843
sean <seansaoirseh@gmail.com>
2020-04-26 20:48:51 +0200

    bug fixed api/reset-password/send

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -8,41 +8,46 @@ const checkUserExists = require("./utils/checkUserExists");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-//@route    POST api/user/send-reset-password
+//@route    POST api/reset-password/send
 //@desc     Sends an email to reset user's password
 //@access  Public
-router.post("/send/:id", async (req, res) => {
-  //   let user = checkUserExists("_id", req.params.id);
+router.post("/send", async (req, res) => {
+  const { email } = req.body;
 
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.NODEMAILER_EMAIL,
-      pass: process.env.NODEMAILER_PASSWORD,
-    },
-  });
+  let user = await checkUserExists("email", email);
+  if (user) {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.NODEMAILER_EMAIL,
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
+    });
 
-  let frontendHost = "http://localhost:3000";
+    let frontendHost = "http://localhost:3000";
 
-  let mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
-    to: "seangeekpro@gmail.com",
-    subject: "Reset Kata Sandi - Layanan Mandiri | Lapor Diri ",
-    html:
-      "Halo! <br> Untuk me-reset kata sandi Anda, mohon kunjungi link berikut: <br> <a href='" +
-      frontendHost +
-      "/reset-kata-sandi/" +
-      req.params.id +
-      "'>reset kata sandi</a>. <br> <br>Link ini akan kedaluwarsa setelah 5 menit.<br><br>Hormat kami,<br><b>Team Lapor Diri</b>",
-  };
+    let mailOptions = {
+      from: process.env.NODEMAILER_EMAIL,
+      to: "seangeekpro@gmail.com",
+      subject: "Reset Kata Sandi - Layanan Mandiri | Lapor Diri ",
+      html:
+        "Halo! <br> Untuk me-reset kata sandi Anda, mohon kunjungi link berikut: <br> <a href='" +
+        frontendHost +
+        "/reset-kata-sandi/" +
+        user._id +
+        "'>reset kata sandi</a>. <br> <br>Link ini akan kedaluwarsa setelah 5 menit.<br><br>Hormat kami,<br><b>Team Lapor Diri</b>",
+    };
 
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      res.status(400).json({ msg: "error: " + err });
-    } else {
-      res.status(200).json({ msg: "success: " + info.response });
-    }
-  });
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        res.status(400).json({ msg: "error: " + err });
+      } else {
+        res.status(200).json({ msg: "success: " + info.response });
+      }
+    });
+  } else {
+    res.status(404).json({ msg: "user not found" });
+  }
 });
 
 module.exports = router;
