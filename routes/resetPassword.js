a657568b6cc3439c6b4c9e853e506640d3856a50
sean <seansaoirseh@gmail.com>
2020-04-27 07:25:20 +0200

    encrypted uid is now parameter for reset password link

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const config = require("config");
 const { check, validationResult } = require("express-validator");
 const auth = require("../middleware/auth");
 const checkUserExists = require("./utils/checkUserExists");
 const nodemailer = require("nodemailer");
+const encryptor = require("simple-encryptor")(process.env.ENCRYPT_SECRET_KEY);
 require("dotenv").config();
 
 //@route    POST api/reset-password/send
@@ -15,6 +15,7 @@ router.post("/send", async (req, res) => {
   const { email } = req.body;
 
   let user = await checkUserExists("email", email);
+
   if (user) {
     let transporter = nodemailer.createTransport({
       service: "gmail",
@@ -24,6 +25,8 @@ router.post("/send", async (req, res) => {
       },
     });
 
+    let uidEncrypted = encryptor.encrypt(user._id);
+
     let mailOptions = {
       from: process.env.NODEMAILER_EMAIL,
       to: "seangeekpro@gmail.com",
@@ -32,20 +35,25 @@ router.post("/send", async (req, res) => {
         "Halo! <br> Untuk me-reset kata sandi Anda, mohon kunjungi link berikut: <br> <a href='" +
         process.env.FRONTEND_HOST +
         "/reset-kata-sandi/" +
-        user._id +
+        uidEncrypted +
         "'>reset kata sandi</a>. <br> <br>Link ini akan kedaluwarsa setelah 5 menit.<br><br>Hormat kami,<br><b>Team Lapor Diri</b>",
     };
 
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
-        res.status(400).json({ msg: "error: " + err });
+        res.status(400).json({
+          msg: "error: " + err,
+        });
       } else {
-        res.status(200).json({ msg: "success: " + info.response });
+        res.status(200).json({
+          msg: "success: " + info.response,
+        });
       }
     });
   } else {
     res.status(404).json({ msg: "user not found" });
   }
 });
+
 
 module.exports = router;
