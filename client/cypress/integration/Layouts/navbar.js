daf3e9cf54af7e94222af59cddddd3dc365e798b
sean <seangeekpro@gmail.com>
2020-05-06 14:18:36 +0200

    checks target url rather than element on the target page

--- client/cypress/integration/Layouts/navbar.js
+++ client/cypress/integration/Layouts/navbar.js
@@ -2,12 +2,18 @@ describe("navbar component", () => {
   it("Should go to login page by clicking login link", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-cy-login-link]").should("exist").click();
-    cy.get(".card-title").contains("Masuk");
+    cy.url().should("include", "/login"); // => true
   });
 
   it("Should go to login page by clicking login link", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-cy-register-link]").should("exist").click();
-    cy.get("[data-cy-title]").contains("Formulir Layanan Mandiri");
+    cy.url().should("include", "/register"); // => true
+  });
+
+  it("Should go to homepage (/)", () => {
+    cy.visit("http://localhost:3000");
+    cy.get(".navbar-brand").should("exist").click();
+    cy.url().should("eq", "http://localhost:3000/"); // => true
   });
 });
